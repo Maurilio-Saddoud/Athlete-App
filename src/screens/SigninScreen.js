9a721c5dd04986cd96daf928913c090609fa594a
Maurilio-Saddoud <masaddoud@davidson.edu>
2021-03-24 09:52:05 -0400

    added a TouchableWithoutFeedback component to sign in screen. enables user to disable keyboard by tapping anywhere on screen.

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text, Button, TextInput } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 import CreateAccountButton from "../components/CreateAccountButton";
 import LoginBar from "../components/LoginBar";
 import LoginButton from "../components/LoginButton";
@@ -9,28 +15,30 @@ const SigninScreen = ({ navigation }) => {
   const [term, setTerm] = useState("");
   const [term2, setTerm2] = useState("");
   return (
-    <View style={styles.viewStyle}>
-      <Text style={styles.kumoStyle}>KUMO</Text>
-      <View style={styles.underlineStyle} />
-      <Text style={styles.metricStyle}>Metrics</Text>
-      <Text style={styles.login}>Login:</Text>
-      <LoginBar
-        text={"Username"}
-        term={term}
-        onTermChange={(newTerm) => setTerm(newTerm)}
-        secure={false}
-      />
-      <LoginBar
-        text={"Password"}
-        term={term2}
-        onTermChange={(newTerm2) => setTerm2(newTerm2)}
-        secure={true}
-      />
-      <Spacer space={30} />
-      <LoginButton />
-      <Spacer space={20} />
-      <CreateAccountButton />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <View style={styles.viewStyle}>
+        <Text style={styles.kumoStyle}>KUMO</Text>
+        <View style={styles.underlineStyle} />
+        <Text style={styles.metricStyle}>Metrics</Text>
+        <Text style={styles.login}>Login:</Text>
+        <LoginBar
+          text={"Username"}
+          term={term}
+          onTermChange={(newTerm) => setTerm(newTerm)}
+          secure={false}
+        />
+        <LoginBar
+          text={"Password"}
+          term={term2}
+          onTermChange={(newTerm2) => setTerm2(newTerm2)}
+          secure={true}
+        />
+        <Spacer space={30} />
+        <LoginButton />
+        <Spacer space={20} />
+        <CreateAccountButton />
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
