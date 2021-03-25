2b4fda60e8a9e7f667469f41a3e6f694751696bd
AndrewDenny16 <andenny@davidson.edu>
2021-03-24 20:33:59 -0400

    update of Code Bar

--- src/components/CodeBar.js
+++ src/components/CodeBar.js
@@ -4,24 +4,27 @@ import { Text, View, TextInput, StyleSheet } from 'react-native';
 const CodeBar = ({text, term, onTermChange}) => {
     return <View style = {styles.codebar}>
         <TextInput style = {styles.input}
-        placeholder = {text} 
+        placeholder = {text}
+        placeholderTextColor = "#8ecfff" 
         autoCapitalize = "none"
         autoCorrect = {false}
         value = {term}
         onChangeText = {(newTerm) => onTermChange(newTerm)}
+        color = "#8ecfff"
         />
         </View>
 }
 
 const styles = StyleSheet.create({
     codebar: {
-        backgroundColor: '#F0EEEE',
+        backgroundColor: 'black',
         height: 50,
         width: 300,
         borderRadius: 5,
-        marginTop: 30,
+        marginTop: 10,
         flexDirection: 'row',
-        
+        borderBottomWidth: 4,
+        borderBottomColor: "#8ecfff"
     },
     input : {
         flex: 1,
