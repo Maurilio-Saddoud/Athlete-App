6306ca873ad0b1473767743f8fb45866ace94f84
AndrewDenny16 <andenny@davidson.edu>
2021-05-17 22:15:26 -0400

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -3,14 +3,14 @@ import "@firebase/auth";
 import "@firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAYhFOG6wzF8C6PqOX6nUvi3N4iTgQjYc0",
-    authDomain: "athlete-app-38bb0.firebaseapp.com",
-    projectId: "athlete-app-38bb0",
-    storageBucket: "athlete-app-38bb0.appspot.com",
-    messagingSenderId: "794713352596",
-    appId: "1:794713352596:web:dd66c867697199c3970781",
-    measurementId: "G-72Q1RTSTBV"
+var firebaseConfig = {
+  apiKey: "AIzaSyDdysB5UcOPund_WWGn2rNMvkepmTOoiVk",
+  authDomain: "kumo-metrics-b43eb.firebaseapp.com",
+  projectId: "kumo-metrics-b43eb",
+  storageBucket: "kumo-metrics-b43eb.appspot.com",
+  messagingSenderId: "675448634834",
+  appId: "1:675448634834:web:30b385ec407f1bb595dc0b",
+  measurementId: "G-KF3D06GWVD"
 };
 
 if (!firebase.apps.length) {
